08e0896e5a5af1819e5065a2caf59d8e64902ab9
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-02-21 02:14:31 -0800

    updated-summary

--- summary.js
+++ summary.js
@@ -1,6 +1,7 @@
 // destructuring object 
 // to extract properties from object and bind them to variables 
 //can extracts multiple properties in one statement
+//can set the default value of missing properties in one statement
 const fish = { name: 'Salmon', age: 34, address: 'sea', dress: 'silver' };
 const { name, age, address } = fish;
 console.log(name, age);
@@ -13,6 +14,23 @@ const company = {
 }
 const { fundamental2, fundamental3 } = company.web;
 console.log(fundamental2, fundamental3);
+//or other way
+const { web: { fundamental2: x, fundamental3: y } } = company;
+console.log(x, y);
+const { fundamental1: a, fundamental2: b } = company.software;
+console.log(a, b);
+// or 
+const { software: { fundamental1: c, fundamental2: d } } = company;
+console.log(c, d);
+//when the properties might be missing we need to set default value ;
+//that returns undefined instead of throwing error
+const { hardware: { fundamental2: e } = {} } = company;/*hardware is missing and programme cannot read properties fundamental3 of undefined*/
+console.log(e);
+//or
+// optional chaining 
+const chaining = company?.hardware?.fundamental2;
+console.log(chaining);
+
 //array destructuring 
 //extracts array elements to distinct variables
 const numbers = [1, 2, 3, 4, 5];
@@ -24,6 +42,16 @@ let second = 2;
 // console.log(first, second);
 [first, second] = [second, first];
 console.log(first, second);
+//uses of array destructuring on nested array
+const nestedArray = [1, 2, 3, 4, [5, 6, 7], 8, 9, 10];
+const [, , , four] = nestedArray;
+console.log(four);
+
+const [, , three, , [, six, ,], , nine, ,] = nestedArray;
+console.log(three, six, nine);
+
+const [, , , , [, , ,], , , ten] = nestedArray;
+console.log(ten);
 
 //optional chaining
 const company1 = {
@@ -82,6 +110,19 @@ console.log(productPrices);
 //also creates a new array and calling function for every elements
 // when we don't need to return array from function;we need to use forEach()
 const product = products.forEach(x => console.log(products));
+const numbers6 = [1, 2, 3, 4, 5, 6];
+const newArray = [];
+const twoTimes = numbers6.forEach(x => {
+    const multiply = x * 2;
+    newArray.push(multiply);
+});
+console.log(newArray);/*map is better than foreach.because map method 1.loop through for every element;2.call the function for every element;3.stored the result in a new array and returns the new array. But foreach method 1.loop through for every element;2.call the function for every element;3.but doesn't stored the result in a new array and it returns undefined;so we need to declare a new array and push the result in this new array.*/
+
+//this calculation in map() method is:
+const twoTimes2 = numbers6.map(x => x * 2);
+console.log(twoTimes2);
+//so that is why map() method is so useful than foreach().
+
 
 //implement filter
 // filter returns a new array from a given array consisting of only those elements from the given array which satisfies a condition that is provided by a function.
@@ -91,7 +132,7 @@ console.log(black);
 
 //find()
 // check the condition and return the only one matched elements
-// filter() returns all the matched elements in an array
+// filter() returns all the matched elements in a new array
 //find() returns just the first matched elements .
 const black1 = products.find(x => x.color == 'black');
 console.log(black1);
